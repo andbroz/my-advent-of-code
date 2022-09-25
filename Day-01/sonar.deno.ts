cc89b211e6a24586d702613e67cd04bbce5f1a4c
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2022-09-25 09:29:50 +0200

    add deno version

---
+++ Day-01/sonar.deno.ts
@@ -0,0 +1,60 @@
+import * as path from 'https://deno.land/std@0.156.0/path/mod.ts';
+const inputFilename = 'input.data.txt';
+
+const file = path.join(Deno.cwd(), 'Day-01', inputFilename);
+
+console.log(file);
+
+async function sonar(filePath: string) {
+  const inputData = await Deno.readTextFile(filePath);
+
+  const arrData = inputData.split('\n').map((strVal) => Number.parseInt(strVal));
+
+  const increasesCount = countDepthIncreaseFromPrevious(arrData);
+
+  const windowsIncCount = slidingWindowIncreaseCount(arrData);
+
+  console.log({ increasesCount, windowsIncCount });
+}
+
+function countDepthIncreaseFromPrevious(data: number[]): number {
+  const result = data.map((val, i, arr) => {
+    if (i === 0) {
+      return 'n/a';
+    }
+
+    const depthChange = val - arr[i - 1];
+    if (depthChange > 0) {
+      return 'inc';
+    } else if (depthChange === 0) {
+      return 'zero';
+    }
+    return 'dec';
+  });
+
+  return result.reduce((acc, val) => {
+    if (val === 'inc') {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+}
+
+function slidingWindowIncreaseCount(data: number[]): number {
+  const windowSumData: number[] = [];
+
+  const dataSize = data.length;
+  for (let i = 0; i + 2 < dataSize; i++) {
+    const a = data[i];
+    const b = data[i + 1];
+    const c = data[i + 2];
+
+    windowSumData.push(a + b + c);
+  }
+
+  return countDepthIncreaseFromPrevious(windowSumData);
+}
+
+console.time('test');
+sonar(file);
+console.timeEnd('test');
